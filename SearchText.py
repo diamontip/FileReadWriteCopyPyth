e39b14959fff723c2754a632fdf470f3955ef6e8
Shanmuga Raja <geekraja@gmail.com>
2012-05-12 13:12:29 +0530

    searching text file for term and print entire line

---
+++ SearchText.py
@@ -0,0 +1,6 @@
+search_file = raw_input("provide file name:")
+search_text = raw_input("Provide the search term:")
+
+print """this script will search for the provided text: %s 
+in the file and returns true or false as result""" %search_text
+
